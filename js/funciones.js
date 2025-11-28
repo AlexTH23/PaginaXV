f8f6634f06b6412ca6afdd8f3796c91fd0bd76d2
AlexTH23 <2236000174@utna.edu.mx>
2025-11-27 19:19:44 -0600

    first commit

---
+++ js/funciones.js
@@ -0,0 +1,36 @@
+
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000 // Cambia el valor para ajustar la velocidad de transición
+  },
+});
+
+
+//FUNCIONES DEL CORREO
+
+const form = document.getElementById('contactForm');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const date = document.getElementById('dateInput').value;
+  const time = document.getElementById('timeInput').value;
+
+  const message = `Name: ${name}\nEmail: ${email}\nDate: ${date}\nTime: ${time}`;
+
+  const mailToLink = `mailto:angelicaseegio26@gmail.com?subject=Reservation%20Request&body=${encodeURIComponent(message)}`;
+  window.location.href = mailToLink;
+});
